a567f726d7d1487de2a030eed923b179ec3cd327
Alex Hamlin <alex@alexhamlin.co>
2025-03-01 20:13:39 -0800

    Use tool lines in go.mod

--- generate.go
+++ generate.go
@@ -1,6 +1,6 @@
 package main
 
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate -provider-name zonefile
+//go:generate go tool tfplugindocs generate -provider-name zonefile
 
 //go:generate rm -rf ./release/sources
-//go:generate go run github.com/google/go-licenses save . --save_path=./release/sources --ignore=github.com/ahamlinman/terraform-provider-zonefile
+//go:generate go tool go-licenses save . --save_path=./release/sources --ignore=github.com/ahamlinman/terraform-provider-zonefile
--- tools.go
+++
@@ -1,8 +0,0 @@
-//go:build tools
-
-package main
-
-import (
-	_ "github.com/google/go-licenses"
-	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
-)
